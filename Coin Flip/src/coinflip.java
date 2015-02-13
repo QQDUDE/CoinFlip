ecddbc48d107adb2b3defc00cdc4fc90da2c7109
QQDUDE <qqdude2017@gmail.com>
2015-02-13 14:45:51 -0700

    :D

---
+++ Coin Flip/src/coinflip.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class coinflip
+	{
+
+	 
+		static int headscounter = 0;
+		static int tailscounter = 0;
+		
+		public static void main(String[] args)
+			{
+			for (int i = 1; i<11; i++);	
+			}
+		int dice1=(int)(Math.random() *2) + 1;
+		switch (dice1)
+	{
+		case 1:
+	
+	}
+
+	System.out.println(i+") Heads");
+	}
+}
